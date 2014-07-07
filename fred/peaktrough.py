dca50d37b70f446be18f2222345d766f723803b0
Spencer Lyon <spencerlyon2@gmail.com>
2014-07-07 11:52:07 -0400

    DOC: updated docstrings

--- fred/peaktrough.py
+++ fred/peaktrough.py
@@ -15,6 +15,28 @@ def chopseries(data, indices, periods=40):
     """
     Takes a series and turns it into a data frame starting at each index
     and running for the number of periods specified (default is 40)
+
+    Parameters
+    ----------
+    data : pd.Series
+        The Series that should be chopped. Index should be a
+        DatetimeIndex
+
+    indices : pd.DatetimeIndex
+        A pandas DatetimeIndex where each item represents the beginning
+        of a cycle
+
+    periods : int, optional(default=40)
+        An integer specifying the maximum number of periods to retain
+        in each cycle. In other words, the function will attempt to keep
+        `periods` items, starting at each date in indices
+
+    Returns
+    -------
+    new_data : pd.DataFrame
+        A pd.DataFrame with columns named for the year the cycle
+        started. The data is a subset of the original series passed into
+        the function
     """
     # Number or series to plot
     n = len(indices)
@@ -40,6 +62,22 @@ def peak_begin_dates(start="01/01/1972", end=datetime.now()):
     """
     Use the fred dataset `USRECQ` to determine the beginning of the
     peaks before all recessions between dates start and end
+
+    Parameters
+    ----------
+    start : string or datetime.datetime, optional(default='01/01/1972')
+        A string or other acceptable pandas date identifier that marks
+        the beginning of the window for which we will search for starts
+        of peaks
+
+    end : string or datetime.datetime, optional(default=datetime.now())
+        The ending date of the search window
+
+    Returns
+    -------
+    rec_startind : pd.DatetimeIndex
+        A pandas DatetimeIndex representing the starting points of each
+        "peak" from start to end
     """
     # Get quarterly recession dates from FRED
     rec_dates = DataReader("USRECQ", "fred", start=start)
@@ -47,7 +85,7 @@ def peak_begin_dates(start="01/01/1972", end=datetime.now()):
     rec_start = [one_vals[0]]
 
     # Find the beginning of the recession dates (Don't include ones that
-    # begin within three years of a previous one)
+    # begin within three years of a previous one -- hence the `+12`)
     for d in one_vals:
         if d > max(rec_start) + 12:
             rec_start.append(d)
@@ -62,10 +100,10 @@ def manhandle_freddata(fred_series, nperiods=40,
                        **plot_kwargs):
     """
     This function takes a string that corresponds to a data series from
-    FRED and creates a dataframe that takes this series and creates a
-    new series that shows the change of the series starting at the
-    beginning of every business cycle and ending nperiods quarters later
-    using either log differences or percent change.
+    FRED and creates a DataFrame that takes this series and creates a
+    new series that shows the percent change of the series starting at
+    the beginning of every business cycle and ending `nperiods` quarters
+    later using either log differences or percent change.
 
     By default it will start at the beginning of 1972 and additionally
     data should be quarterly
@@ -73,6 +111,44 @@ def manhandle_freddata(fred_series, nperiods=40,
     If you would like to use multiple series, use python's map function:
 
         map(manhandle_freddata, [list of fred_series])
+
+    Parameters
+    ----------
+    fred_series : string
+        A string representing the fred dataset identifier.
+
+    nperiods : int, optional(default=40)
+        The number of periods each cycle should represent. This is
+        passed directly to the `chopseries` function
+
+    changetype : string
+        A string identifying how the percentage change should be
+        computed. Acceptable values are `percent` or `log`
+
+    start : string or datetime.datetime, optional(default='01/01/1972')
+        A string or other acceptable pandas date identifier that marks
+        the beginning of the window for which we will search for starts
+        of peaks. This is passed directly to `pd.io.data.DataReader` to
+        obtain the data set and to `peak_begin_dates` to determine
+        starting periods for business cycle peaks
+
+    plot_kwargs : other
+        Other keyword arguments that will be passed directly to the
+        `pd.DataFrame.plot` method when generating the plot. See pandas
+        plotting documentation for an explanation of acceptable values
+
+    Returns
+    -------
+    pct_change : pd.DataFrame
+        The pandas DataFrame representing representing the percent
+        change from the beginning of each peak, extended out `nperiods`
+
+    Examples
+    --------
+    >>> rgdp = manhandle_freddata('GDPC1')  # Real GDP. Produces plot
+
+    For more examples see the `examples.ipynb` notebook in this
+    directory.
     """
     # Get data
     fred_data = DataReader(fred_series, "fred", start=start)
